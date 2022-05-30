b19bcdf3c94f8f7f9537b0490ac92510533dcac8
robertuptc <robertuptc@gmail.com>
2022-05-30 11:50:28 -0400

    finished exercise

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,33 @@
-exports.charCount = function() {
+exports.charCount = function(str) {
+    let splitString = str.replace(/[" "]/g,"").split('');
+    let obj = {};
+    let answer = []    ;
+   
+    for (let i = 0; i < splitString.length; i++) {
+        if (!obj.hasOwnProperty([splitString[i]])) {
+            obj[splitString[i]] = 1;
+        } else {
+            obj[splitString[i]] += 1;
+        }
+    } 
+    
+    let arrOfObj = Object.keys(obj).map(e => ({letter : e, count : obj[e]}))
 
+    let sortDirection = -1
+    sortedObj = arrOfObj.slice().sort(function(a,b) {
+        if (a.count > b.count) {return sortDirection}     
+        if (a.count < b.count) {return -sortDirection}
+        if (a.count == b.count) {
+            if (a.letter < b.letter) {return -1}
+            if (a.letter > b.letter) {return 1}
+        }
+    })
+
+    for (let i = 0; i < sortedObj.length; i++) {
+        answer.push(Object.values(sortedObj[i]))
+    }
+    return answer
 };
+
+
+
--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,29 @@
-def char_count(str):
-  # Your code here
+from typing import ItemsView
+from functools import cmp_to_key
+
+def char_count(string):
+  split_string = sorted(string.replace(" ", ""))
+  dictionary_count = {}
+
+  for i in range(0, len(split_string), 1):
+      if (split_string[i]) in dictionary_count :
+        dictionary_count[split_string[i]] += 1
+      else :
+        dictionary_count[split_string[i]] = 1
+
+  list_of_dict = []
+
+  for key in dictionary_count :
+      list_of_dict.append(  [key,dictionary_count[key]]    )
+
+  def sort_by_count(a,b): 
+    if a[1] - b[1] > 0: 
+      return -1
+    elif a[1] - b[1] < 0: 
+      return 1
+    elif a[1] == b[1]:
+      return 1 if a[0] > b[0] else -1
+
+  sorted_dict = sorted(list_of_dict, key = cmp_to_key(sort_by_count))
+
+  return sorted_dictld
